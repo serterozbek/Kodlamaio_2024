79406380c64ac853bae0976b3e9d567e571caba2
Serter Özbek <serter.ozbek@hotmail.com>
2024-02-05 22:52:22 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,35 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[2];
+    //constructor
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "C#";
+        course1.Description = ".NET 8";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course2.Id = 2;
+        course2.Name = "JAVA";
+        course2.Description = "JAVA 17....";
+        course2.Price = 10;
+
+        courses[0] = course1;
+        courses[1] = course2;
+    }
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+// variables --> camelCase
+
+bool isAuthenticated = false;
+Console.WriteLine(message1);
+
+
+//condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Buton --> Hoşgeldin Serter");
+}
+else
+{
+    Console.WriteLine("Buton --> Sisteme Giriş Yapınız.");
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3" };
+/* string[] loans2 = new string[]
+{
+    "Kredi 1",
+    "kredi 2",
+    "kredi 3"
+};
+
+*/
+
+
+/*
+string[] loans3 = new string[6];
+loans3[0] = "Kredi1";
+loans3[1] = "Kredi2";
+loans3[2] = "Kredi3";
+loans3[3] = "Kredi4";
+*/
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + "/" + courses2[i].Price);
+}
