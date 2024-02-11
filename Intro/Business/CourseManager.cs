8be1729ce8e0121a63e94c056feefacde2123111
Serter Özbek <serter.ozbek@hotmail.com>
2024-02-11 22:09:56 +0300

    Day2, lesson 2, commit 1

--- Intro/Business/CourseManager.cs
+++ Intro/Business/CourseManager.cs
@@ -1,4 +1,5 @@
-﻿using Intro.Entities;
+﻿using Intro.DataAccess.Concretes;
+using Intro.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,27 +10,12 @@ namespace Intro.Business;
 
 public class CourseManager
 {
-    Course[] courses = new Course[2];
-    //constructor
-    public CourseManager()
-    {
-        Course course1 = new Course();
-        course1.Id = 1;
-        course1.Name = "C#";
-        course1.Description = ".NET 8";
-        course1.Price = 0;
 
-        Course course2 = new Course();
-        course2.Id = 2;
-        course2.Name = "JAVA";
-        course2.Description = "JAVA 17....";
-        course2.Price = 10;
-
-        courses[0] = course1;
-        courses[1] = course2;
-    }
-    public Course[] GetAll()
+    public List<Course> GetAll()
     {
-        return courses;
+        // business rules
+        CourseDal courseDal = new CourseDal();
+
+        return courseDal.GetAll();
     }
 }
